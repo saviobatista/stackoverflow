06dec69e20658fcadd0e63f23b2896951633ab3c
Sávio Batista <saviobatista@email.com>
2022-04-02 22:30:22 -0300

    feat: headless chromium, whastapp notification

--- scraper.py
+++ scraper.py
@@ -1,14 +1,23 @@
+import logging
 import os
 from time import sleep
 from dotenv import load_dotenv
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
+from requests import get
+from datetime import datetime
 
 if __name__ == '__main__':
     try:
         load_dotenv()
-        driver = webdriver.Chrome()
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        # options.add_experimental_option("detach", True)
+        driver = webdriver.Chrome(options=options,service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM,log_level=logging.ERROR).install()))
         driver.get('https://stackoverflow.com/users/login?ssrc=head&returnurl=https%3a%2f%2fstackoverflow.com%2f')
         assert 'Log In - Stack Overflow' in driver.title
         # sleep(1)
@@ -21,12 +30,22 @@ if __name__ == '__main__':
         assert 'Stack Overflow - Where Developers Learn, Share, & Build Careers' in driver.title
         # driver.find_element(by=By.XPATH, value='/html/body/header/div/ol[2]/li[2]/a').click()
         driver.execute_script('return document.evaluate(\'/html/body/header/div/ol[2]/li[2]/a\',document,null,XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click()')
+        sleep(2)
+        reputacao = driver.find_element(by=By.XPATH, value='/html/body/header/div/ol[2]/li[2]/a/div[2]/ul/li[1]/span').text
+        medals = driver.find_element(by=By.XPATH, value='/html/body/header/div/ol[2]/li[2]/a/div[2]/ul/li[2]/span[2]').text
         driver.close()
-        print('Feito.')
+        url = 'https://api.callmebot.com/whatsapp.php'
+        param = {
+            'phone':os.environ.get('PHONE'),
+            'text':'Robo stackoverflow em ' + datetime.today().strftime ('%d/%m/%Y') + '\nMedalhas: ' + medals + reputacao,
+            'apikey':os.environ.get('APIKEY')
+        }
+        r = get(url = url, params = param)
     except WebDriverException as e:
-        print('ERRO FATAL!')
-        print(e.args)
-        print(e.msg)
-        print(e.stacktrace)
-        print(e.screen)
-        print('-')
+        url = 'https://api.callmebot.com/whatsapp.php'
+        param = {
+            'phone':os.environ.get('PHONE'),
+            'text':'Robo stackoverflow em ' + datetime.today().strftime ('%d/%m/%Y') + '\n\nHouve um erro no robo do stackoverflow: ' + str(e),
+            'apikey':os.environ.get('APIKEY')
+        }
+        r = get(url = url, params = param)
